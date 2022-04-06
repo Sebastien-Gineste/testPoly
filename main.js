455b8719b5f1f7ab1624b158136a1d5cb2646b06
Sebastien-Gineste <47150778+Sebastien-Gineste@users.noreply.github.com>
2022-04-06 16:28:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log(stack 1);
-console.log(stack 3);
-console.log(unstack);
-console.log(stack 2);
+console.log("stack 1");
+console.log("stack 3");
+console.log("unstack");
+console.log("stack 2");
